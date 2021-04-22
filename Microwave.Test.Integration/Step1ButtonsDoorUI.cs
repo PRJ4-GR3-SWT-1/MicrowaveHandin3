765296e2496dab06262b52efd02514f116c758ff
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-22 13:09:38 +0200

    Setup for step1

--- Microwave.Test.Integration/Step1ButtonsDoorUI.cs
+++ Microwave.Test.Integration/Step1ButtonsDoorUI.cs
@@ -1,12 +1,35 @@
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
     public class Step1ButtonsDoorUI
     {
+        private Button startCancelButton;
+        private Button powerButton;
+        private Button timeButton;
+        private Door door;
+        private UserInterface ui;
+        private IDisplay display;
+        private ILight light;
+        private ICookController cookController;
         [SetUp]
         public void Setup()
         {
+            //Create instances:
+            startCancelButton = new Button();
+            powerButton = new Button();
+            timeButton = new Button();
+            door = new Door();
+            //Create Substitutes
+            display=Substitute.For<IDisplay>();
+            light=Substitute.For<ILight>();
+            cookController=Substitute.For<ICookController>();
+            //Assemble instances to UI
+            ui = new UserInterface(powerButton, timeButton, startCancelButton, door, display, light, cookController);
         }
 
         [Test]
